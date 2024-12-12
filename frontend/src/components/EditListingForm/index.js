440d9c7adb4ff439d5a5acf4c38387923f6037de
vivitruong <vivitruong1506@gmail.com>
2024-12-11 21:07:10 -0600

    add useEffect

--- frontend/src/components/EditListingForm/index.js
+++ frontend/src/components/EditListingForm/index.js
@@ -29,3 +29,66 @@ const EditListingForm = ({ listingId, returnToListing }) => {
     const [disableButton, setDisableButton] = useState(false)
 
     const categories = ['A-Frame', 'Amazing Pool', 'Beach', 'Cabin', 'Design', 'Dome', 'Luxe', 'Treehouse', 'Tiny Home', 'Tropical']
+
+    useEffect(() => {
+        const errors = [];
+        if (type.trim().length < 2) errors.push("Type of place between 3 and 30 characters required")
+        if (name.trim().length < 10) errors.push("Title must be between 10 and 50 characters")
+        if (address.trim().length < 6) errors.push("Valid address required")
+        if (city.trim().length < 4) errors.push("Valid city required")
+        if (state.trim().length < 4) errors.push("Valid state required")
+        if (country.trim().length < 4) errors.push("Valid country required")
+        if (lat === "" || lat > 90 || lat < -90) errors.push("Latitude must be between - 90 to 90")
+        if (lng === "" || lng > 180 || lng < -180) errors.push("Longitude must be between - 180 to 180")
+        if (description.trim().length < 10) errors.push("Description required between 10 and 1000 characters")
+        if (price > 1000000 || price < 1) errors.push("Price must be between $1 and $1,000,000")
+
+        if (errors.length > 0) {
+          setErrors(errors)
+          setDisableButton(true)
+        } else {
+          setErrors([])
+          setDisableButton(false)
+        }
+
+      }, [type, name, address, city, state, country, lat, lng, description, price])
+
+
+      const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const roomData = {
+          roomId,
+          ownerId,
+          address,
+          city,
+          state,
+          country,
+          lat,
+          lng,
+          name,
+          description,
+          price,
+          type,
+          category,
+          guests,
+          beds,
+          bedrooms,
+          baths
+        }
+
+        const response = await dispatch(updateRoom(roomData))
+          .catch(async (res) => {
+            const data = await res.json();
+            if (data && data.errors)
+              if (data) {
+                const errors = Object.values(data.errors)
+                setErrors(errors)
+              }
+          })
+
+        if (response) {
+          dispatch(findRoomById(listingId))
+          return returnToListing()
+        }
+      }
